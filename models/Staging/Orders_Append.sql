f7d60e1f61d3626c698ecd148c2046e78c8af980
Nawab Malik <nawabmaliktop10@gmail.com>
2025-02-15 22:04:35 +0000

    c

--- models/Staging/Orders_Append.sql
+++ models/Staging/Orders_Append.sql
@@ -3,4 +3,4 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','raw_orders')}} 
+select * from {{source('datafeed_shared_schema','raw_orders')}} where id in (1,2,3)
